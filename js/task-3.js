f6e0fe275b472fe945bab9edab7eb87467280d31
tharkusha <vladreviakin@ukr.net>
2024-02-24 21:25:37 +0000

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ function filterArray(numbers, value) {
     const filteredArray = []
     for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
-            array.push(numbers[i])
+            filteredArray.push(numbers[i])
         }
     }
-    return array
+    return filteredArray
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
